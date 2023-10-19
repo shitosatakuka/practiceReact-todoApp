f6ee14fc79587ae90c798989dfa10a564f8fefbc
kt3t4 <satoshi.kaku@gmail.com>
2023-10-19 10:11:08 +0000

    Update index.html and index.mjs todoに入れて、追加を押すと未完了リストにテキストが追加される。

--- src/index.mjs
+++ src/index.mjs
@@ -7,7 +7,28 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   //   追加ボタンが押されたら入力内容を初期化する
   document.getElementById("add-text").value = "";
-  alert(inputText);
+  // alert(inputText);
+
+  // liタグの生成
+  const li = document.createElement("li");
+  li.className = "incomplete-item";
+  // console.log(li);
+
+  // divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // console.log(div);
+
+  // pタグの生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  // console.log(p);
+
+  // 入れ子にする
+  li.appendChild(div).appendChild(p);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 // idを取得して、クリックのイベントが発生したらonClickAddの関数が実行される。
 document
