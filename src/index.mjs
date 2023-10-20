33a6eb00fc80509e03202c3bed508dd96d7e1366
kt3t4 <satoshi.kaku@gmail.com>
2023-10-20 01:21:27 +0000

    Update index.html and index.mjs 削除と完了ボタンの設定。完了ボタンから完了したTODOへ移動

--- src/index.mjs
+++ src/index.mjs
@@ -29,8 +29,50 @@ const onClickAdd = () => {
   btnComplete.innerText = "完了";
   // 完了ボタンが押されたら
   btnComplete.addEventListener("click", () => {
-    alert("完了されました");
+    // alert("完了されました");
+    // 完了ボタンが押されたら<li class="incomplete-item">のタグを削除
+    deleteFromIncompleteList(btnComplete.closest("li"));
+    // 下は関数化したので削除。↑は関数。
+    // 完了ボタンが押されたら<li class="incomplete-item">のタグを削除
+    // const deleteTaret = btnComplete.closest("li");
+    // // console.log(deleteTaret);
+    // document.getElementById("incomplete-list").removeChild(deleteTaret);
+
+    // 完了リストに追加する要素を取得
+    const addTarget = btnComplete.parentNode;
+    // TODO内容テキストを取得
+    const textMemo = addTarget.firstElementChild.innerText;
+    console.log(textMemo);
+
+    // div以下を初期化
+    addTarget.textContent = null;
+    // console.log(addTarget);
+
+    // liタグの生成
+    const li = document.createElement("li");
+    li.className = "complete-item";
+    // console.log(li);
+
+    // pタグの生成
+    const p = document.createElement("p");
+    p.innerText = textMemo;
+    console.log(p);
+    // // divタグの中にtextMemoを追加
+
+    // button(戻る)タグ生成
+    const btnBack = document.createElement("button");
+    btnBack.innerText = "戻す";
+
+    // liタグの子要素に設定する。
+    li.appendChild(addTarget);
+    addTarget.appendChild(p);
+    addTarget.appendChild(btnBack);
+    // console.log(li);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(li);
   });
+
   // button(削除)タグ生成
   const btnDelete = document.createElement("button");
   btnDelete.innerText = "削除";
@@ -38,9 +80,11 @@ const onClickAdd = () => {
   btnDelete.addEventListener("click", () => {
     // alert("削除されました");
     // 削除ボタンが押されたら<li class="incomplete-item">のタグを削除
-    const deleteTaret = btnDelete.closest("li");
-    // console.log(deleteTaret);
-    document.getElementById("incomplete-list").removeChild(deleteTaret);
+    deleteFromIncompleteList(btnDelete.closest("li"));
+    // 下は関数化したので削除。↑は関数。
+    // const deleteTaret = btnDelete.closest("li");
+    // // console.log(deleteTaret);
+    // document.getElementById("incomplete-list").removeChild(deleteTaret);
   });
 
   // 入れ子にする。親にappendChildする。同じ親なら同じ子になる。
@@ -50,6 +94,11 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除する関数
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 // idを取得して、クリックのイベントが発生したらonClickAddの関数が実行される。
 document
